1d6a1ebb898d4f5b70ae1a66b1c1d29d3126a62f
zltkhn <zltkhn@gmail.com>
2024-06-24 23:54:16 -0300

    Lesson 1

---
+++ lesson_1/01_ideal_weight.rb
@@ -0,0 +1,10 @@
+puts "Как вас зовут?"
+name = gets.chomp
+puts "Какой у вас рост?"
+height = gets.chomp.to_i
+ideal_weight = (height-110)*1.15
+if ideal_weight < 0
+    puts "#{name}, ваш вес уже оптимальный!" 
+else
+puts "#{name}, ваш идеальный вес #{ideal_weight}"
+end
---
+++ lesson_1/04_quadratic_equation.rb
@@ -0,0 +1,20 @@
+print " Коэффициент a: "
+a = gets.chomp.to_f
+print "Коэффициент b: "
+b = gets.chomp.to_f
+print "Коэффициент c: "
+c = gets.chomp.to_f
+puts 
+D = b**2 - 4*a*c
+puts "Дискриминант: #{D}, корней нет." if D<0
+
+if D > 0
+  puts "Дискриминант: #{D}"
+  puts "Корень x1: #{(-b - Math.sqrt(D))/(2*a)}"
+  puts "Корень x2: #{(-b - Math.sqrt(D))/(2*a)}"
+elsif D == 0
+    puts "Дискриминант: #{D}"
+    puts "Корень: #{(-b)/(2*a)}"
+
+end
+
